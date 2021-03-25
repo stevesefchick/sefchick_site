84d5df7b74c12741425cb154641fd2df2fc95ef5
stevesefchick <stevesefchick@gmail.com>
2021-03-24 23:29:07 -0400

    started working on 2.0 site

---
+++ src/MainNav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const MainNav = (props) => {
+return (
+<div className="mainnav">
+   aw yeah main nav baybeeee
+</div>
+ );
+};
+export default MainNav;
---
+++ src/SideNav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const SideNav = (props) => {
+return (
+<div className="sidenav">
+   <a href="#section">one</a>
+   <a href="#section">two</a>
+   <a href="#section">three</a>
+   <a href="#section">four</a>
+</div>
+ );
+};
+export default SideNav;
