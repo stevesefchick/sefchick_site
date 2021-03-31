302f330d39b1f3063ba6ee63390f9b6f3df451a2
stevesefchick <stevesefchick@gmail.com>
2021-03-31 16:20:35 -0400

    updated menu and routed home page formatting

--- src/SideNav.js
+++ src/SideNav.js
@@ -9,16 +9,39 @@ return (
    <ul className="nav-menu-items">
       <li key="home" className="nav-text">
          <Link to='/home'>
-            <span>heck</span>
+            <span>about</span>
          </Link>
       </li>
 
       <li key="home2" className="nav-text">
-         <Link to='./MainNav.js'>
-            <span>dang</span>
+         <Link to='./home2'>
+            <span>experience</span>
          </Link>
       </li>
 
+      <li key="home3" className="nav-text">
+         <Link to='./home3'>
+            <span>projects</span>
+         </Link>
+      </li>
+
+      <li key="home4" className="nav-text">
+         <Link to='./home4'>
+            <span>skillset</span>
+         </Link>
+      </li>
+
+      <li key="home5" className="nav-text">
+         <Link to='./home5'>
+            <span>education</span>
+         </Link>
+      </li>
+
+      <li key="home6" className="nav-text">
+         <Link to='./home6'>
+            <span>contact</span>
+         </Link>
+      </li>
    </ul>
 </nav>
 
